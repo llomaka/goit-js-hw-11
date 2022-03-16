efd3fa89aee7a1c4ec04c8571ee5c31f6f480e87
Lilia Lomaka <llomaka80@gmail.com>
2022-03-16 14:47:22 +0200

    Додавання посилання на pixabay

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import { Notify } from 'notiflix';
 import axios from "axios";
 
 const API_KEY = '24511799-4a7f974650a4e56ef46644e1e';
-const URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('cats');
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-    console.log('No hits');
-});
+const URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('cats')+"&image_type=photo&orientation=horizontal&safesearch=true";
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
+
+// webformatURL - посилання на маленьке зображення для списку карток.
+// largeImageURL - посилання на велике зображення.
+// tags - рядок з описом зображення. Підійде для атрибуту alt.
+// likes - кількість лайків.
+// views - кількість переглядів.
+// comments - кількість коментарів.
+// downloads - кількість завантажень.
+
+// Notify.failure('Sorry, there are no images matching your search query. Please try again.');
