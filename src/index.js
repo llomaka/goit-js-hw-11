7ea5f95a52b749568e4d3c8e885a730521f9b0ae
Lilia Lomaka <llomaka80@gmail.com>
2022-04-18 21:50:56 +0300

    Подвійний запит при гортанні

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import { renderGallery } from './js/renderGallery';
 import getRefs from './js/getRefs';
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { addBackToTop } from 'vanilla-back-to-top'
 
 const STORAGE_KEY = 'gallery-page-number';
 const STORAGE_SEARCH = 'search-end';
 const refs = getRefs();
-let searchValue;
-let lightboxGallery;
+let searchValue = '';
+let lightboxGallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 Notify.init({ fontSize: '18px', timeout: 7000});
 
 refs.button.addEventListener('click', onSubmit);
+addBackToTop();
 
 function onSubmit(event) {
   event.preventDefault();
   const value = refs.input.value.trim();
   searchValue = value;
   refs.gallery.innerHTML = '';
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
   localStorage.removeItem(STORAGE_SEARCH);
   localStorage.removeItem(STORAGE_KEY);
   if (!value) {
@@ -31,43 +38,45 @@ function onSubmit(event) {
       if (response.data.totalHits === 0)
         return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
-      localStorage.removeItem(STORAGE_KEY);
-      localStorage.removeItem(STORAGE_SEARCH);
       renderGallery(response);
-      if (response.data.totalHits < 40)
+      if (response.data.totalHits < 40) {
         localStorage.setItem(STORAGE_SEARCH, 1);
+      }
     })
     .catch(onSearchError);
-  lightboxGallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+  lightboxGallery.refresh();
+  window.addEventListener('scroll', onScroll);
   event.target.form.reset();
 };
 
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
-      if (localStorage.getItem(STORAGE_SEARCH))
-        return Notify.failure("We're sorry, but you've reached the end of search results.");;
-      let pageCounter = 1;
-      if (localStorage.getItem(STORAGE_KEY)) {
-        pageCounter = localStorage.getItem(STORAGE_KEY);
-      }
-      fetchPictures(searchValue, Number(pageCounter) + 1)
-        .then(response => {
-          if (!response.data.hits.length) {
-            localStorage.removeItem(STORAGE_KEY);
-            localStorage.setItem(STORAGE_SEARCH, 1);
-            return Notify.failure("We're sorry, but you've reached the end of search results.");
-          }
-          let numbers = response.request.responseURL.match(/\d+/g);
-          localStorage.setItem(STORAGE_KEY, numbers[numbers.length - 1]);
-          renderGallery(response);
-          lightboxGallery.refresh();
-        })
-        .catch(onSearchError);
+function onScroll() {
+  if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+    if (localStorage.getItem(STORAGE_SEARCH)) {
+      window.removeEventListener('scroll', onScroll);
+      return Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+    let pageCounter = 1;
+    if (localStorage.getItem(STORAGE_KEY)) {
+      pageCounter = localStorage.getItem(STORAGE_KEY);
+    }
+    fetchPictures(searchValue, Number(pageCounter) + 1)
+      .then(response => {
+        if (!response.data.hits.length) {
+          localStorage.removeItem(STORAGE_KEY);
+          localStorage.setItem(STORAGE_SEARCH, 1);
+          window.removeEventListener('scroll', onScroll);
+          return Notify.failure("We're sorry, but you've reached the end of search results.");
+        }
+        let numbers = response.request.responseURL.match(/\d+/g);
+        localStorage.setItem(STORAGE_KEY, numbers[numbers.length - 1]);
+        renderGallery(response);
+        lightboxGallery.refresh();
+      })
+      .catch(onSearchError);
   }
-});
-
+}
 
 function onSearchError(error) {
   Notify.failure(error);
-  console.log(error);
+  console.error(error);
 }
