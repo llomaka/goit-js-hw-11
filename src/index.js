17915479a5aad6833f4f29544283637387f1aa63
Lilia Lomaka <llomaka80@gmail.com>
2022-04-18 23:14:51 +0300

    Доопрацювання оновлення галереї

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const STORAGE_KEY = 'gallery-page-number';
 const STORAGE_SEARCH = 'search-end';
 const refs = getRefs();
 let searchValue = '';
-let lightboxGallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+const lightboxGallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 Notify.init({ fontSize: '18px', timeout: 7000});
 refs.more.classList.add("visually-hidden");
 refs.button.addEventListener('click', onSubmit);
@@ -36,43 +36,39 @@ function onSubmit(event) {
         return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
       Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
       renderGallery(response);
+      lightboxGallery.refresh();
       if (response.data.totalHits < 40) {
         Notify.warning("We're sorry, but you've reached the end of search results.");
         localStorage.setItem(STORAGE_SEARCH, 1);
-        refs.more.classList.add("visually-hidden");
       }
       if (response.data.totalHits > 40) {
         refs.more.classList.remove("visually-hidden");
       }
-
     })
     .catch(onSearchError);
-  lightboxGallery.refresh();
   event.target.form.reset();
 };
 
 function onClick() {
-    if (localStorage.getItem(STORAGE_SEARCH)) {
-      return Notify.warning("We're sorry, but you've reached the end of search results.");
-    }
-    let pageCounter = 1;
-    if (localStorage.getItem(STORAGE_KEY)) {
-      pageCounter = localStorage.getItem(STORAGE_KEY);
-    }
-    fetchPictures(searchValue, Number(pageCounter) + 1)
-      .then(response => {
-        if (!response.data.hits.length) {
-          localStorage.removeItem(STORAGE_KEY);
-          localStorage.setItem(STORAGE_SEARCH, 1);
-          refs.more.classList.add("visually-hidden");
-          return Notify.failure("We're sorry, but you've reached the end of search results.");
-        }
-        let numbers = response.request.responseURL.match(/\d+/g);
-        localStorage.setItem(STORAGE_KEY, numbers[numbers.length - 1]);
-        renderGallery(response);
-        lightboxGallery.refresh();
-      })
-      .catch(onSearchError);
+  if (localStorage.getItem(STORAGE_SEARCH))
+    return Notify.warning("We're sorry, but you've reached the end of search results.");
+  let pageCounter = 1;
+  if (localStorage.getItem(STORAGE_KEY))
+    pageCounter = localStorage.getItem(STORAGE_KEY);
+  fetchPictures(searchValue, Number(pageCounter) + 1)
+    .then(response => {
+      if (!response.data.hits.length) {
+        localStorage.removeItem(STORAGE_KEY);
+        localStorage.setItem(STORAGE_SEARCH, 1);
+        refs.more.classList.add("visually-hidden");
+        return Notify.warning("We're sorry, but you've reached the end of search results.");
+      }
+      let numbers = response.request.responseURL.match(/\d+/g);
+      localStorage.setItem(STORAGE_KEY, numbers[numbers.length - 1]);
+      renderGallery(response);
+      lightboxGallery.refresh();
+    })
+    .catch(onSearchError);
 }
 
 function onSearchError(error) {
